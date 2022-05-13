2c55219121d667157857bd134a337ab4e95c453d
Riley Anne Reid <rileyannereid@lair12.int.colorado.edu>
2022-05-13 08:03:37 -0600

    added generator for pt src

--- include/PrimaryGeneratorAction.hh
+++ include/PrimaryGeneratorAction.hh
@@ -31,12 +31,13 @@
 #ifndef PrimaryGeneratorAction_h
 #define PrimaryGeneratorAction_h 1
 
+#include "G4GeneralParticleSource.hh"
 #include "G4ParticleGun.hh"
 #include "G4VUserPrimaryGeneratorAction.hh"
 #include "globals.hh"
 
-class G4ParticleGun;
-class PrimaryGeneratorMessenger;
+// class G4ParticleGun;
+class G4GeneralParticleSource;
 class G4Event;
 class G4Box;
 
@@ -45,43 +46,20 @@ class G4Box;
 /// The default kinematic is a 6 MeV gamma, randomly distribued
 /// in front of the phantom across 80% of the (X,Y) phantom size.
 
-struct ParticleSample {
-  G4double x, y, z;
-  G4double xDir, yDir, zDir;
-  G4double energy;
-};
-
 class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
 public:
   PrimaryGeneratorAction();
   virtual ~PrimaryGeneratorAction();
 
   // method from the base class
-  virtual void GeneratePrimaries(G4Event *anEvent);
+  virtual void GeneratePrimaries(G4Event *);
 
-  // Statistical selections of part. energy, position, and direction
-  void GenerateElectrons(ParticleSample *);
-
-  // Sets background and signal folding energies
-  void SetFoldingEnergy(G4double E0) { fE_folding = E0; };
-
-  // Sets background spatial distribution
-  void SetPitchAngleDistribution(G4int type) { fPitchAngleDist = type; };
-
-  // Method to access particle gun
-  const G4ParticleGun *GetParticleGun() const { return fParticleGun; }
+  // // method to access particle gun
+  const G4GeneralParticleSource *GetParticleGun() const { return fParticleGun; }
 
 private:
-  G4ParticleGun *fParticleGun; // pointer a to G4 gun class
-
-  G4double fE_folding;
-  G4double fPI;
-  G4double fSphereR;
-
-  G4int fPitchAngleDist;
-
-  G4ParticleDefinition *fElectronParticle;
-  PrimaryGeneratorMessenger *fPrimaryGeneratorMessenger;
+  // pointer a to G4 gun class
+  G4GeneralParticleSource *fParticleGun;
 };
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
